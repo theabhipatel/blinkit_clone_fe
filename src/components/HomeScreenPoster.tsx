e2df3d2f12f65e0b9cd569b1dd15595bc6324f85
TheAbhiPatel <theabhipatel.co@gmail.com>
2023-12-10 12:22:02 +0530

    Added home screen poster

---
+++ src/components/HomeScreenPoster.tsx
@@ -0,0 +1,17 @@
+const HomeScreenPoster = () => {
+  return (
+    <div className="home-poster ">
+      <div>
+        <img src="/home-screen-poster.webp" alt="home screen poster" />
+      </div>
+
+      <div className="flex gap-4 px-4 my-3">
+        <img src="/pharmacy-cat.avif" alt="pharmacy-poster" className="h-40" />
+        <img src="/petcare.avif" alt="petcare-poster" className="h-40" />
+        <img src="/babycare-cat.avif" alt="babycare-poster" className="h-40" />
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreenPoster;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
+import HomeScreenPoster from "../components/HomeScreenPoster";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="w-full px-20">
+      <HomeScreenPoster />
+    </main>
+  );
 };
 
 export default Home;
