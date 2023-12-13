f0eb898fb0b6ce3f06774b50d2a93d08ae5bee7b
TheAbhiPatel <theabhipatel.co@gmail.com>
2023-12-13 12:59:14 +0530

    Design PageNotFound page has been designed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Search from "./pages/Search";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/s" element={<Search />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+const PageNotFound = () => {
+  return (
+    <div className="w-full h-screen flex gap-5 justify-center items-center ">
+      <img src="/not-found.webp" alt="not-found-image" className="h-60" />
+      <div className="flex flex-col gap-3 text-xs">
+        <h3 className="text-2xl font-bold">Oops!</h3>
+        <h4 className="text-base ">
+          The page you are looking for can’t be found.
+        </h4>
+        <h5 className="text-zinc-400">You might find these links useful</h5>
+        <div className="flex flex-col gap-1 ">
+          <NavLink to={"/"} className="text-green-600">
+            Go to Home
+          </NavLink>
+          <NavLink to={"/"} className="text-green-600">
+            Learn about blinkit
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
