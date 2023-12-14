15f61dcba154d260079ebe667c42b95d344a9468
TheAbhiPatel <theabhipatel.co@gmail.com>
2023-12-14 10:51:26 +0530

    Add constant data

---
+++ src/constant/index.ts
@@ -0,0 +1,211 @@
+interface IWhyShopFromBlinkit {
+  title: string;
+  desc: string;
+  image: string;
+}
+
+export const whyShopFromBlinkit: IWhyShopFromBlinkit[] = [
+  {
+    title: "Superfast Delivery",
+    desc: "Get your order delivered to your doorstep at the earliest from dark stores near you.",
+    image: "/10-minute-delivery.avif",
+  },
+  {
+    title: "Best Prices & Offers",
+    desc: "Best price destination with offers directly from the manufacturers.",
+    image: "/best-prices-offers.avif",
+  },
+  {
+    title: "Wide Assortment",
+    desc: "Choose from 5000+ products across food, personal care, household & other categories.",
+    image: "/wide-assortment.avif",
+  },
+];
+
+interface IUsefullLinks {
+  title: string;
+  link: string;
+}
+
+export const usefullLinks: IUsefullLinks[] = [
+  {
+    title: "About",
+    link: "",
+  },
+  {
+    title: "Privacy",
+    link: "",
+  },
+  {
+    title: "Partner",
+    link: "",
+  },
+  {
+    title: "Careers",
+    link: "",
+  },
+  {
+    title: "Terms",
+    link: "",
+  },
+  {
+    title: "Express",
+    link: "",
+  },
+  {
+    title: "Blog",
+    link: "",
+  },
+  {
+    title: "FAQs",
+    link: "",
+  },
+  {
+    title: "Seller",
+    link: "",
+  },
+  {
+    title: "Press",
+    link: "",
+  },
+  {
+    title: "Security",
+    link: "",
+  },
+  {
+    title: "Spotlight",
+    link: "",
+  },
+  {
+    title: "Lead",
+    link: "",
+  },
+  {
+    title: "Mobile",
+    link: "",
+  },
+  {
+    title: "Warehouse",
+    link: "",
+  },
+  {
+    title: "Value",
+    link: "",
+  },
+  {
+    title: "Contact",
+    link: "",
+  },
+  {
+    title: "Deliver",
+    link: "",
+  },
+];
+
+interface ICategories {
+  title: string;
+  link: string;
+}
+
+export const categories: ICategories[] = [
+  {
+    title: "Vegetables & Fruits",
+    link: "",
+  },
+  {
+    title: "Dairy & Breakfast",
+    link: "",
+  },
+  {
+    title: "Munchies",
+    link: "",
+  },
+  {
+    title: "Cold Drinks & Juices",
+    link: "",
+  },
+  {
+    title: "Instant & Frozen Food",
+    link: "",
+  },
+  {
+    title: "Tea, Coffee & Health Drinks",
+    link: "",
+  },
+  {
+    title: "Bakery & Biscuits",
+    link: "",
+  },
+  {
+    title: "Sweet Tooth",
+    link: "",
+  },
+  {
+    title: "Atta, Rice & Dal",
+    link: "",
+  },
+  {
+    title: "Dry Fruits, Masala & Oil",
+    link: "",
+  },
+  {
+    title: "Sauces & Spreads",
+    link: "",
+  },
+  {
+    title: "Atta, Rice & Dal",
+    link: "",
+  },
+  {
+    title: "Paan Corner",
+    link: "",
+  },
+  {
+    title: "Organic & Premium",
+    link: "",
+  },
+  {
+    title: "Baby Care",
+    link: "",
+  },
+  {
+    title: "Pharma & Wellness",
+    link: "",
+  },
+  {
+    title: "Cleaning Essentials",
+    link: "",
+  },
+  {
+    title: "Home & Office",
+    link: "",
+  },
+  {
+    title: "Ice Creams & Frozen Desserts",
+    link: "",
+  },
+  {
+    title: "Personal Care",
+    link: "",
+  },
+  {
+    title: "Pet Care",
+    link: "",
+  },
+  {
+    title: "Beauty & Cosmetics",
+    link: "",
+  },
+  {
+    title: "Toys & Games",
+    link: "",
+  },
+  {
+    title: "Trial new tree",
+    link: "",
+  },
+  {
+    title: "Print Store",
+    link: "",
+  },
+];
