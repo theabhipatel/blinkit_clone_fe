cdc2f7965173606606a15583341dc8a9166bca86
TheAbhiPatel <theabhipatel.co@gmail.com>
2023-12-15 15:36:29 +0530

    Fix public path in search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -5,11 +5,7 @@ const Search = () => {
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center ">
-      <img
-        src="/public/not-found.webp"
-        alt="not-found-image"
-        className="h-52"
-      />
+      <img src="/not-found.webp" alt="not-found-image" className="h-52" />
       <h3 className="text-4xl mt-5 font-semibold text-zinc-400">
         Nothing here yet
       </h3>
