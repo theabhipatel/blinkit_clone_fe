e16d2b4815e49b17e0b4624525096c6471d1afcf
TheAbhiPatel <theabhipatel.co@gmail.com>
2023-12-23 12:57:47 +0530

    Fix overflow issue of home screen posters

--- src/components/HomeScreenPoster.tsx
+++ src/components/HomeScreenPoster.tsx
@@ -5,7 +5,7 @@ const HomeScreenPoster = () => {
         <img src="/home-screen-poster.webp" alt="home screen poster" />
       </div>
 
-      <div className="flex gap-4 px-4 my-3">
+      <div className="flex gap-4 px-4 my-3 overflow-scroll hide-scrollbar">
         <img src="/pharmacy-cat.avif" alt="pharmacy-poster" className="h-40" />
         <img src="/petcare.avif" alt="petcare-poster" className="h-40" />
         <img src="/babycare-cat.avif" alt="babycare-poster" className="h-40" />
