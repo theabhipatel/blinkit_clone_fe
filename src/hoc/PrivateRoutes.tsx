7c426c82e96c27a6d9c1d1f6f4e9d3b3438c3bf6
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-01-07 12:01:20 +0530

    Fix bug- in private routes on reload page navigating to home

--- src/hoc/PrivateRoutes.tsx
+++ src/hoc/PrivateRoutes.tsx
@@ -1,11 +1,10 @@
 import { FC } from "react";
-import { useAppSelector } from "../store/hooks";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoutes: FC = () => {
-  const isUserLoggedIn = useAppSelector((state) => state.auth.isUserLoggedIn);
+  const token = localStorage.getItem("@accessToken");
 
-  return isUserLoggedIn ? <Outlet /> : <Navigate to={"/"} />;
+  return token ? <Outlet /> : <Navigate to={"/"} />;
 };
 
 export default PrivateRoutes;
