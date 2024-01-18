b9394a683437d5c04a589cb6e8f7215990eaf74f
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-01-18 21:06:47 +0530

    Feat added map in save address modal

--- src/modals/SaveAdressModal.tsx
+++ src/modals/SaveAdressModal.tsx
@@ -57,7 +57,16 @@ const SaveAdressModal = () => {
           onClick={handlePreventClick}
           className="relative w-[43rem] h-[26rem] bg-white rounded-r-xl flex  items-center overflow-hidden"
         >
-          <div className="map w-[45%] h-full bg-blue-500"></div>
+          <div className="map w-[45%] h-full">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12378.569217728606!2d75.86037957133615!3d22.721897831892914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fd1246ae310b%3A0x2bf544615bbb4407!2sGandhi%20Hall!5e0!3m2!1sen!2sin!4v1705592105543!5m2!1sen!2sin"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
           <div className="adress w-[55%] h-full p-5 ">
             <div className="flex justify-between items-center">
               <h3 className=" font-bold">Enter complete address</h3>
