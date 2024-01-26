ffa61a6217d30ae1aa9d83826ef132077298ac1c
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-01-26 14:07:11 +0530

    Redesign my order page

--- src/components/MyOrders.tsx
+++ src/components/MyOrders.tsx
@@ -5,20 +5,22 @@ import { getMyOrdersAsync } from "../store/user/userSlice";
 const MyOrders = () => {
   const dispatch = useAppDispatch();
   const orders = useAppSelector((state) => state.user.orders);
-  console.log("orders --->", orders);
+
   useEffect(() => {
     dispatch(getMyOrdersAsync());
   }, []);
 
   return (
-    <div className="w-full h-full p-4 ">
+    <div className="w-full h-full p-2 md:p-4 ">
       {orders.length === 0 ? (
-        <h2 className="text-xl font-bold text-center">
-          Oops, you haven't placed an order yet.
-        </h2>
+        <div className="w-full h-[90vh] flex justify-center items-center md:items-start px-5 ">
+          <h2 className="md:text-xl font-bold text-center">
+            Oops, you haven't placed an order yet.
+          </h2>
+        </div>
       ) : (
         <div className="w-full">
-          <h2 className="text-lg font-semibold mb-4">My orders</h2>
+          <h2 className="text-lg font-semibold mb-2 md:mb-4">My orders</h2>
           {orders.map((item) => {
             const {
               courtesyTitle,
@@ -28,24 +30,30 @@ const MyOrders = () => {
               addressLine2,
             } = item.selectedAddress;
             return (
-              <div key={item._id} className="border-t p-3">
+              <div
+                key={item._id}
+                className="md:border-t p-2 md:p-3 my-2 bg-gray-50 md:bg-white rounded-md md:rounded-none"
+              >
                 <div className="flex justify-between">
-                  <h3 className="text-sm font-semibold">
+                  <h3 className="text-sm font-semibold text-zinc-600">
                     {courtesyTitle} {name}
                   </h3>
                   <h4 className="text-sm font-semibold">
                     Amt : ₹{item.totalAmount}
                   </h4>
                 </div>
-                <p className="text-xs mt-1 text-zinc-500 line-clamp-1">
+                <p className="text-xxs mt-1 text-zinc-500 line-clamp-1">
                   <span className="font-semibold"> {addressType} : </span>
                   {addressLine1}, {addressLine2}
                 </p>
                 <div className="flex justify-between text-xs text-zinc-500 font-semibold mt-2 ">
                   <div>
-                    <p>Payment Status : success</p>
+                    <p className="text-xxs md:text-xs">
+                      Payment Status :{" "}
+                      <span className="text-green-500">success</span>
+                    </p>
                   </div>
-                  <div className="flex gap-2 items-center">
+                  <div className="flex gap-2 items-center text-xxs md:text-xs">
                     Order Status :{" "}
                     {Date.now() >
                     new Date(item.createdAt).valueOf() + 10 * 1000 * 60 ? (
